f5574d780957c2a3ef6ac3d54c422bade5b5a730
meazaasres <83246678+meazaasres@users.noreply.github.com>
2026-03-03 02:01:10 +0300

    Revert "update svg export"
    This reverts commit ced7537f1dffa88f2d24837dfc6e6e30174ebae4.

--- app.js
+++ app.js
@@ -4304,7 +4304,7 @@ function exportSVG() {
           )
         );
         const legendX = Math.max(
-          marginPx,
+          contentOffsetXPx + marginPx,
           Math.round((totalWidthPx - estimatedLegendWidth) / 2)
         );
         let yOff = titleHeightPx + usedCanvasHeight + marginPx;
