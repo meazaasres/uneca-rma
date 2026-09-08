8eb5a97df1ad24047619646b00f13507f103b603
meazaasres <meazct@gmail.com>
2026-09-08 12:06:06 +0300

    Revert "Update app.js"
    This reverts commit d9427dbcce9f072c920ac21d224fa61ba955c514.

--- app.js
+++ app.js
@@ -5664,6 +5664,8 @@ window.addEventListener('load', resetInitialScrollPositions);
       const height = Math.max(1, mapCanvas.height);
       clone.setAttribute('width', String(width));
       clone.setAttribute('height', String(height));
+      clone.setAttribute('viewBox', `0 0 ${width} ${height}`);
+      clone.setAttribute('preserveAspectRatio', 'none');
       clone.style.transform = 'none';
       clone.style.position = 'absolute';
       clone.style.left = '0';
